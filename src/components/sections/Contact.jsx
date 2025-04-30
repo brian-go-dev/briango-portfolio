102b7d93a21fcf9548b03fd269a90bf87f093efa
Brian Go <83991275+Brian-Go-Developer@users.noreply.github.com>
2025-04-29 20:55:11 -0500

    minor css changes

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -30,10 +30,10 @@ export const Contact = () => {
   return (
     <section
       id="contact"
-      className="min-h-screen flex items-center justify-center py-29"
+      className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="w-full sm:w-full md:w-[200px] px-4 md:px-10 mx-auto">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             Get In Touch
           </h2>
